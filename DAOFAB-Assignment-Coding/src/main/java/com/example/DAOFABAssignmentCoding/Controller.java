7f384cb71a53d805c0edfdc56fc6030f05f00592
Rashid Khalil <rashidkhalil855@gmail.com>
2023-05-30 19:50:04 +0500

    Update Controller.java

--- DAOFAB-Assignment-Coding/src/main/java/com/example/DAOFABAssignmentCoding/Controller.java
+++ DAOFAB-Assignment-Coding/src/main/java/com/example/DAOFABAssignmentCoding/Controller.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://coding-task-front-end.vercel.app/"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://rashidkhalil9182.github.io/daofeb-coding-assignment/"})
 public class Controller {
     private final ObjectMapper mapper = new ObjectMapper();
 
